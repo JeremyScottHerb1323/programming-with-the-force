9dc64f0a2ddcf4f0f740a8de356f7babd521ec93
Jeremy Herb <jeremys387@gmail.com>
2014-11-19 20:13:31 -0500

    Initial Commit
    Already has some basics here.

---
+++ includes/_header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html class="no-js">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+
+  <?= get_stylesheets() ?>
+</head>
+<body class="sw-bg">
+  <!--[if lt IE 8]>
+    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+  <![endif]-->
+  <header>
+    <section>
+      <div class="ta-center opening">
+        <h1>Programming With The Force</h1>
+        <p>My name, Jeremy Herb.. my profession, jedi mind fucking..</p>
+      </div>
+    </section>
+  </header>
+  <div id="content">
+    <section>
