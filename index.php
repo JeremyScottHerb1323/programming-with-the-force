9bee2b6ebe62bb2a43af0aa8fd6913f873d5992c
Jeremy Herb <jeremys387@gmail.com>
2014-12-08 22:21:47 -0500

    Slight Update
    Re-working layout a bit
    - need to get an overall feel for the site.

--- index.php
+++ index.php
@@ -2,52 +2,40 @@
   require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/functions.php";
   render('header');
 ?>
-<div>
-<ul class="ch-grid">
-  <li>
-    <div class="ch-item ch-img-1">
-      <div class="ch-info">
-        <h3>Use what you have</h3>
-        <p>by Angela Duncan <a href="http://drbl.in/eOPF">View on Dribbble</a></p>
-      </div>
-    </div>
-  </li>
-  <li>
-    <div class="ch-item ch-img-2">
-      <div class="ch-info">
-        <h3>Common Causes of Stains</h3>
-        <p>by Antonio F. Mondragon <a href="http://drbl.in/eKMi">View on Dribbble</a></p>
-      </div>
-    </div>
-  </li>
-  <li>
-    <div class="ch-item ch-img-3">
-      <div class="ch-info">
-        <h3>Pink Lightning</h3>
-        <p>by Charlie Wagers <a href="http://drbl.in/ekhp">View on Dribbble</a></p>
-      </div>
-    </div>
-  </li>
- </ul>
- </div>
- <div class="full cf">
-  <div id="ribbon" class="left one-quarter rebel cf">
-    <div class="inset"></div>
-    <div class="container">
-      <div class="base"></div>
-      <div class="left_corner"></div>
-      <div class="right_corner"></div>
-    </div>
+<div class="intro-container full">
+  <div class="intro cf right">
+    <h3>Are you a Star Wars fan?</h3>
+    <p>Are you a fan of the Star Wars franchise? Maybe you enjoy the darkside of things or prefer to be a rebel?</p>
+    <img class="left" src="/images/ui/vader.jpg" alt="">
   </div>
-    <div id="ribbon" class="right one-quarter empire cf">
-    <div class="inset"></div>
-    <div class="container">
-      <div class="base"><img src="/images/icons/empire-150.png" alt="Empire"></div>
-      <div class="left_corner"></div>
-      <div class="right_corner"></div>
-    </div>
+  <div class="intro">
+    <h3>Love Programming?</h3>
+    <p>Do you enjoy programming? Perhaps you enjoy a little Ruby or perhaps some JavaScript?</p>
   </div>
+  <div class="intro">
+    <h3>Wanna be part of the programming community?</h3>
+    <p>Wanna join a community of developers and collaborate on awesome open source projects?</p>
+  </div>
+</div>
+
+   <div class="full cf">
+    <div id="ribbon" class="left one-quarter rebel cf">
+      <div class="inset"></div>
+      <div class="container">
+        <div class="base"></div>
+        <div class="left_corner"></div>
+        <div class="right_corner"></div>
+      </div>
     </div>
+      <div id="ribbon" class="right one-quarter empire cf">
+      <div class="inset"></div>
+      <div class="container">
+        <div class="base"><img src="/images/icons/empire-150.png" alt="Empire"></div>
+        <div class="left_corner"></div>
+        <div class="right_corner"></div>
+      </div>
+    </div>
+      </div>
  <?php render('tie-fight'); ?>
 
 <?php render('footer'); ?>
